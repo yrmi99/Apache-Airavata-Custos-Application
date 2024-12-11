d0ba75c01e6091f9bc172068809b35249cc7482c
kripakannan <135384830+kripakannan@users.noreply.github.com>
2024-12-10 23:43:43 -0500

    remove function

--- custos/src/generated2/group_pb.d.ts
+++ custos/src/generated2/group_pb.d.ts
@@ -212,8 +212,3 @@ export class Empty extends jspb.Message {
   static deserializeBinary(bytes: Uint8Array): Empty;
   static deserializeBinaryFromReader(message: Empty, reader: jspb.BinaryReader): Empty;
 }
-
-export namespace Empty {
-  export type AsObject = {
-  }
-}
