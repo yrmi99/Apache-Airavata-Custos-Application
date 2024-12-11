706a6530b05103b3039c5280cb2d83965a4d0b94
kripakannan <135384830+kripakannan@users.noreply.github.com>
2024-12-10 23:44:20 -0500

    removed vulnerabilities

--- custos/src/generated2/group_pb.d.ts
+++ custos/src/generated2/group_pb.d.ts
@@ -212,3 +212,8 @@ export class Empty extends jspb.Message {
   static deserializeBinary(bytes: Uint8Array): Empty;
   static deserializeBinaryFromReader(message: Empty, reader: jspb.BinaryReader): Empty;
 }
+
+export namespace Empty {
+  export type AsObject = {
+  }
+}
